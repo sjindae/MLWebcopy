06d35888ba214b2a2197ac567fab5cad12082a55
sjindae <tnwjd9113@naver.com>
2022-04-15 19:05:06 +0900

    용량 큰 거 올리지마라

--- app.py
+++ app.py
@@ -9,6 +9,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/mnist',methods=['GET','POST'])
+def mnist():
+    if request.method == 'GET':
+        return render_template('mnistform.html')
+    else:
+        f = request.files['mnistfile']
+        path = os.path.dirname(__file__)+'/upload/'+f.filename
+        f.save(path)
+        img = Image.open(path).convert('L')
+        img = np.resize(img,(1,784))
+        mpath = os.path.dirname(__file__)+'/model1.pickle'
+        with open(mpath, 'rb') as f:
+            model = pickle.load(f)
+        pred = model.predict(img)
+        return render_template('mnistresult.html',data=pred)
 
 
 
