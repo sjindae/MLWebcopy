5691b918d59ddfa77cf57b25e94089e69d128038
sjindae <tnwjd9113@naver.com>
2022-04-15 15:10:25 +0900

    default.html

--- app.py
+++ app.py
@@ -2,3 +2,16 @@ from flask import Flask,render_template,request
 import os,pickle
 from PIL import Image
 import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+
+
+if __name__ == '__main__':    # 이게 몰까유?
+    app.run(debug=True)
